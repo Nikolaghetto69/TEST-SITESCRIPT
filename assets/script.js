b1e818aaa4a52be4f2ffa1006ee517fda56b7a0f
Nikolaghetto69 <106533699+Nikolaghetto69@users.noreply.github.com>
2025-12-14 09:43:55 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+let typed = new Typed('.tp', {
+  // Waits 1000ms after typing "First"
+  strings: ['Топовый ^1000 Дизайнер', 'Лучший Разработчик','Создатель Игр','Отец русских Сайтов','Босс KFS'],
+  typeSpeed: 150,
+  loop:true
+});
+
+let next= document.querySelector(.next)
+
+next.addEventlistener(click , function)
+let items = document.querySelector(item)
+document.querySelector(slide).appendchild(items 0)
+
+prev.addEventlistener(click , function)
+let items = document.querySelector(item)
+document.querySelector(slide).appendchild(items 0)
+
